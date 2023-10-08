43ba23de670e41019ee28d8fb007d23ccd21dedf
fzxlzy <38089446+fzxlzy@users.noreply.github.com>
2023-10-08 11:48:59 +0800

    Update README.tsx

--- README.tsx
+++ README.tsx
@@ -1,46 +1,54 @@
 import React, { Component } from 'react';
-import { View, Button } from 'react-native';
-import * as Animatable from 'react-native-animatable';
+import { View, Text, TouchableOpacity, Animated } from 'react-native';
 
 class AnimatedViewDemo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      scale: 1,
+      scaleValue: new Animated.Value(1), // 初始化为 1，表示原始大小
     };
   }
 
-  // 当点击按钮时，放大视图
+  // 放大动画
   scaleUp = () => {
-    this.setState({
-      scale: 1.5, // 放大倍数
-    });
+    Animated.timing(this.state.scaleValue, {
+      toValue: 1.5, // 放大倍数
+      duration: 500, // 动画持续时间（毫秒）
+      useNativeDriver: false, // 不使用原生动画驱动
+    }).start();
   };
 
-  // 当点击按钮时，还原视图大小
+  // 还原大小动画
   resetScale = () => {
-    this.setState({
-      scale: 1, // 还原到原始大小
-    });
+    Animated.timing(this.state.scaleValue, {
+      toValue: 1, // 还原到原始大小
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
   };
 
   render() {
-    const { scale } = this.state;
+    const { scaleValue } = this.state;
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Animatable.View
-          animation="zoomIn" // 使用 'zoomIn' 动画
-          duration={500} // 动画持续时间（毫秒）
-          style={{ width: 150, height: 150, backgroundColor: 'lightblue' }}
-          useNativeDriver // 使用原生动画驱动（提高性能）
-          transition={['scale']} // 监听 scale 属性的变化
-          style={{ transform: [{ scale }] }} // 通过 transform 属性添加放大效果
+        <Animated.View
+          style={{
+            width: 150,
+            height: 150,
+            backgroundColor: 'lightblue',
+            transform: [{ scale: scaleValue }], // 使用 transform 属性添加放大效果
+          }}
         >
           {/* 这里放置你要放大的内容 */}
-          <Button title="放大" onPress={this.scaleUp} />
-        </Animatable.View>
-        <Button title="还原" onPress={this.resetScale} />
+          <Text>放大的内容</Text>
+        </Animated.View>
+        <TouchableOpacity onPress={this.scaleUp}>
+          <Text>放大</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.resetScale}>
+          <Text>还原</Text>
+        </TouchableOpacity>
       </View>
     );
   }
